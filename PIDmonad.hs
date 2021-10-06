6cba5f97870aee97625d72976e9e6d6a5b1f798a
bhargavshivkumar <bhargavs@buffalo.edu>
2021-10-06 01:36:20 -0400

    edits

--- PIDmonad.hs
+++ PIDmonad.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 import Control.Monad
 
 newtype Sensorval v = Sensorval v deriving (Eq, Ord)
@@ -16,7 +17,7 @@ class (Num v, Monad m, Monad p) => PIDMonad m v p | p->m, p->v where
     control :: (v,v,v) -> p v
     apply :: (v,v,v) -> p ()
     lift :: m a -> p a
-    run :: p a -> Sensorval v -> m a
+    run :: p a ->Sensorval v ->  m a
 
 
 class (Ord v , Num v, Monad m) => HasSensor m v where
@@ -51,6 +52,28 @@ instance (Monad m, HasSensor m v, Applicative (PA m v)) => PIDMonad m v (PA m v)
     control (kp, ki, kd) = PA $ \s -> let (PA m1) = drift in m1 s >>= \ (PS i d v, e) -> return (PS i d v , (kp*e)+(ki*i)+(kd*d))
     apply (kp, ki, kd) = PA $ \s -> let (PA m1) = control (kp,ki,kd) in m1 s >>= \(s1,c) -> controlActuator c >>= \a -> return (s1,a)
     lift m = PA $ \s -> m >>= \a -> return (s,a)
-    run (PA m) sp = m (PS 0 0 sp) >>= \ (_,a) -> return a
+    --run (PA m)  =m (PS 0 0 (Sensorval 1)) >>= \ (_,a) -> return a
+    run (PA m) sp = let loop s = do { (ss,_) <- m s; loop ss} in loop (PS 0 0 sp)
 
+type PIO = PA IO Double
+
+{- runSt :: Monad m => PA m v a -> PS v -> m (PS v, a)
+runSt (PA m) s = m s >>= \(s1,a) -> return (s1,a)
+
+execST :: (Monad m, Num v) => PA m v a -> PS v -> m (PS v)
+execST p s = liftM fst (runSt p s)
+
+loop :: (HasSensor m v, Applicative (PA m v)) => PS v -> m b
+loop s = do
+    { ss <- execST (apply (1,2,3)) s; loop ss}
+
+initState :: Num v => Sensorval v -> PS v
+initState sp = PS 0 0 sp
+
+doit :: (HasSensor m v, Applicative (PA m v)) => Sensorval v -> m b
+doit sp = loop (initState sp) -}
+
+doit2 sp g =  run (apply g) sp
+
+main :: IO ()
 main = putStrLn "hello World"
