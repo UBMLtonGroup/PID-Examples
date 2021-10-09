7290b234adbc273da398d83233325602e29fd7e6
bhargavshivkumar <bhargavs@buffalo.edu>
2021-10-09 17:05:50 -0400

    edits

--- PIDmonad.hs
+++ PIDmonad.hs
@@ -7,9 +7,9 @@
 module PIDmonad where
 import Control.Monad
 import Control.Applicative (Applicative)
+import Control.Monad.Fix
 
 import Debug.Trace ( trace )
-import Control.Monad.Writer.Strict (Monad)
 
 newtype Sensorval v = Sensorval v deriving (Eq, Ord)
 
@@ -18,7 +18,7 @@ diff (Sensorval v1) (Sensorval v2) = v2 - v1
 
 class (Num v, Monad m, Monad p) => PIDMonad m v p | p->m, p->v where
     current :: p (Sensorval v)
-    setSP :: Sensorval v -> p ()
+    setSP ::  v -> p ()
     drift :: p v
     control :: (v,v,v) -> p v
     apply :: (v,v,v) -> p ()
@@ -58,29 +58,35 @@ instance (Monad m, HasSensor m v) => Monad (PA m v) where
 
 instance (Monad m, HasSensor m v, Applicative (PA m v)) => PIDMonad m v (PA m v) where
     current = PA $ \s -> let PS _ _ v = s in return (s,v)
-    setSP n = PA $ \s -> let PS i d _ = s in return (PS i d n, ())
-    drift = PA $ \s -> let PS i d v = s in getDiff v >>= \e -> let PS i d v = s in return (PS (i+e) (e-d) v , e)
-    --control (kp, ki, kd) = PA $ \s -> let PS i d v = s in getDiff v >>= \e -> let PS i d v = s in
-        --return (PS (i+e) (e-d) v , (kp*e)+(ki*i)+(kd*d))
-    control (kp, ki, kd) = PA $ \s -> let (PA m1) = drift in m1 s >>= \ (PS i d v, e) -> return (PS i d v , (kp*e)+(ki*i)+(kd*d))
+    setSP n = PA $ \s -> let PS i d _ = s in return (PS i d (Sensorval n), ())
+    drift = PA $ \s -> let PS i d v = s in getDiff v >>= \e -> return (s , e)
+    control (kp, ki, kd) = PA $ \s -> let PS i d v = s in getDiff v >>= \e -> let PS i d v = s in
+        return (PS (i+e) e v , (kp*e)+(ki*i)+(kd*d))
+    --control (kp, ki, kd) = PA $ \s -> let (PA m1) = drift in m1 s >>= \ (PS i d v, e) -> return (PS i d v , (kp*e)+(ki*i)+(kd*d))
     apply (kp, ki, kd) = PA $ \s -> let (PA m1) = control (kp,ki,kd) in m1 s >>= \(s1,c) -> controlActuator c >>= \a -> return (s1,a)
     lift m = PA $ \s -> m >>= \a -> return (s,a)
     run (PA m) sp =m (PS 0 0 sp) >>= \ (_,a) -> return a
-    -- run (PA m) sp = let loop s = do { (ss,_) <- m s; trace "." loop ss} in loop (PS 0 0 sp)
+    --run (PA m) sp = let loop s = do { (ss,_) <- m s; trace "." loop ss} in loop (PS 0 0 sp)
+    --run (PA m) sp = flip fix (1,PS 0 0 sp) $ \loop (n,s) -> do {(ss,a)<- m s; putStrLn "hi" >>  loop (n+1,ss)}
     --run = runTo 0
 
+{- 
 step :: (Integral n, Monad m, HasSensor m v) => n -> PA m v a -> PS v -> m (PS v, a)
 step 0 (PA m) s = m s
 step n (PA m) s = step (n-1) (PA m) s >>= m . fst
 
 runTo :: (Integral n, HasSensor m v) => n -> PA m v a -> Sensorval v -> m a
 runTo n a sp = step n a (PS 0 0 sp) >>= return . snd
-
+ -}
 rep :: (Integral n, Monad m, HasSensor m v) => n -> PA m v () -> PA m v ()
 rep 1 x = x
 rep n a = a *> rep (n-1) a
 
 type PIO = PA IO Double
+
+instance HasSensor PIO Double where
+    getSensorVal = lift getSensorReading
+    controlActuator = setSP
 
 {- runSt :: Monad m => PA m v a -> PS v -> m (PS v, a)
 runSt (PA m) s = m s >>= \(s1,a) -> return (s1,a)
@@ -98,7 +104,30 @@ initState sp = PS 0 0 sp
 doit :: (HasSensor m v, Applicative (PA m v)) => Sensorval v -> m b
 doit sp = loop (initState sp) -}
 
-doit2 sp g =  run (apply g) sp
+-- ### Using only apply
+primary :: PA PIO Double ()
+primary = apply (1,2,3)
+
+secondary :: PA IO Double ()
+secondary = apply (1,2,3)
+
+--cascade s = let PA m1 =primary in m1 s >>= \(PS i d v,_) ->  let PA m2 = secondary in m2 (PS 0 0 v) >>= \(_,a) -> return a
+
+-- ## using control and apply
+primaryC :: PA IO Double Double
+primaryC = control (1,2,3)
+
+secondaryC :: Double -> PA IO Double ()
+secondaryC sp= PA $ \s -> let (PA m1) = setSP sp in m1 s >>= \ (s1,_) -> let PA m = apply (1,2,3) in m s1 
+
+cascade :: (Monad m, HasSensor m v) => PA m v a -> (a -> PA m v b) -> PS v -> PS v -> m b
+cascade (PA m) f s1 s2 = m s1 >>= \(s1',c) -> let (PA m2) = f c in m2 s2 >>= \(s2',a) -> return a
+
+test = cascade primaryC secondaryC 
+
+
+--doit2 :: (Monad m, HasSensor m v) => Sensorval v -> (v, v, v) -> m ()
+--doit2 sp g =  run (apply g) sp
 
 main :: IO ()
 main = putStrLn "hello World"
